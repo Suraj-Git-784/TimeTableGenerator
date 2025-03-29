201b761ca76fad93c854bedd432e66fdd8ed6930
surajkumar <surajkumarele.2019@gmail.com>
2025-03-29 17:57:58 +0530

    First cmmit added model for time table

---
+++ TimeTableGenerator/Models/TimeTableModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTableGenerator.Models
+{
+    public class TimeTableModel
+    {
+        [Required, Range(1, 7, ErrorMessage = "Please enter days between 1 to 7.")]
+        public int WorkDays { get; set; }
+
+        [Required, Range(1, 9, ErrorMessage = "Please enter subjects between 1 to 9.")]
+        public int Periods { get; set; }
+
+        [Required(ErrorMessage = "Please enter total number of subjects.")]
+        public int TotalSubjects { get; set; }
+
+        public int TotalPeriods => WorkDays * Periods;
+    }
+}
